69f0206a8c3c0d4b73ada35a73c2ec555102d27b
lt7-7 <969031921@qq.com>
2020-03-26 09:37:51 +0800

    logs

---
+++ JavaEE-03/src/main/java/org/example/javaee/class03/jdbc/DatabasePool.java
@@ -0,0 +1,31 @@
+package org.example.javaee.class03.jdbc;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class DatabasePool {
+    private static HikariDataSource hikariDataSource;
+
+    public static HikariDataSource getHikariDataSource() {
+
+        if (hikariDataSource != null) {
+            return hikariDataSource;
+        }
+
+        synchronized (DatabasePool.class) {
+            if (hikariDataSource == null) {
+                String url = "jdbc:mysql://127.0.0.1:3306/school?serverTimezone=Asia/Shanghai";
+                HikariConfig hikariConfig = new HikariConfig();
+                hikariConfig.setUsername("root");
+                hikariConfig.setPassword("123456");
+                hikariConfig.setJdbcUrl(url);
+                hikariDataSource = new HikariDataSource(hikariConfig);
+                return hikariDataSource;
+            }
+        }
+
+        return null;
+
+    }
+
+}
